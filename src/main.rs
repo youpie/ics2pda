a7682ae1fd190efc09c4e304634439cb37769d5e
youpie <youplamb@hotmail.nl>
2024-10-03 18:03:53 +0200

    Initial testing

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,61 @@
+use dotenvy::dotenv_override;
+use dotenvy::var;
+use icalendar::Calendar;
+use icalendar::Event;
+
 type GenResult<T> = Result<T, Box<dyn std::error::Error>>;
 
-struct mip_calendar {
+struct MipCalendar {
     hash: i128,
     filename: String,
-    calendar: icalendar::Calendar,
+    calendar_entry: icalendar::Calendar,
 }
 
-impl mip_calendar {
-    fn new() -> GenResult<Self> {
-        Self {}
-    }
+// impl MipCalendar {
+//     fn new() -> GenResult<Self> {
+//         Ok(Self {})
+//     }
+// }
+
+fn parse_calendar_env(env_string: String) -> Vec<String> {
+    let string_split: Vec<&str> = env_string.split(';').collect();
+    let owned_split: Vec<String> = string_split.into_iter().map(|x| x.to_owned()).collect();
+    owned_split
 }
 
-fn parse_calendar_env() {}
-
-fn main() {
-    parse_calendar_env();
-    load_new_entries();
+#[tokio::main]
+async fn main() {
+    dotenv_override().ok();
+    let calendar_string = var("calendar_list").unwrap();
+    let calendar_list = parse_calendar_env(calendar_string);
+    load_new_entries(calendar_list).await.unwrap();
 }
 
-fn load_new_entries(calendars: Vec<String>) -> GenResult<()> {
+async fn load_new_entries(calendars: Vec<String>) -> GenResult<()> {
     for calendar in calendars {
-        let entries = load_calendar(calendar);
-        for entry in entries {
-            add_ids_to_description();
-            add_category();
-            create_hash();
-            generate_filenames();
-            save_to_disk();
-        }
+        let entries = load_calendar(calendar).await?;
+        let test = entries.into_iter().nth(0).unwrap();
+        dbg!(test);
+        // for entry in entries {
+        //     add_ids_to_description();
+        //     add_category();
+        //     create_hash();
+        //     generate_filenames();
+        //     save_to_disk();
+        // }
     }
+    Ok(())
 }
 
-fn load_calendar(calendars: String) {}
+async fn load_calendar(calendar_url: String) -> GenResult<Calendar> {
+    // dbg!(&calendar_url);
+    let mut ics_data = reqwest::get(calendar_url).await?.text().await?;
+    ics_data = ics_data.replace("\n\r", "\n");
+    println!("{}", &ics_data);
+    let parsed_calendar = icalendar::parser::read_calendar(&ics_data)?;
+    parsed_calendar.print();
+    Ok(parsed_calendar.into())
+}
 
 fn add_ids_to_description() {}
 
